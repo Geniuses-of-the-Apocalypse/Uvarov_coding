b38e8d8dcb127a066169f6c9d7e054f6ac9037b1
Kop4ik41 <kostyauvarov1@gmail.com>
2026-09-21 11:05:55 +0300

    Update 1.py

--- FunctionPrograming/1.py
+++ FunctionPrograming/1.py
@@ -1,6 +1,4 @@
 import csv
-from typing import Optional
-
 
 def parse_csv(data: str) -> list[dict]:
     return list(csv.DictReader(data.splitlines()))
@@ -20,16 +18,14 @@ data = """date,item,quantity,price
 2026-09-04,Marker,10.0,2.8
 2026-09-05,Folder,8.0,3.5"""
 
-rows = parse_csv(data)
-
 print("Данные:")
-print(rows)
+print(parse_csv(data))
 
-print("\nОбщая выручка:", compute_revenue(rows))
+print("\nОбщая выручка:", compute_revenue(parse_csv(data)))
 
-# topp_item = top_item(rows)
+# topp_item = top_item(parse_csv(data))
 # max_rev = float(topp_item['quantity']) * float(topp_item['price'])
 # print(f"Максимальная выручка: {max_revenue}")
 
 print("\nТовар с максимальной выручкой:")
-print(top_item(rows))
+print(top_item(parse_csv(data)))
